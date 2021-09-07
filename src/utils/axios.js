e984cee5977f57f6feb885281dc0c1e5c6f897de
felipe parra <felipe.parra@magnotechnology.com>
2021-09-07 01:49:49 -0500

    [fix] cors

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,6 +3,12 @@ import config from "../config";
 
 const instance = axios.create({
   baseURL: config.apiUrl,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  crossdomain: true,
 });
 
 export default instance;
